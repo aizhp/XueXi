ccddd26ce3a2cada572aafcbd02e1fcb53bda3fa
aizhp <aizhp@users.noreply.github.com>
2017-08-05 22:41:17 +0800

    xuexi
    1.0

---
+++ src/XueIf/TestIf.java
@@ -0,0 +1,16 @@
+package XueIf;
+
+public class TestIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 30;
+		if(i<=20){
+			System.out.println("i大于等于20");
+		}else{
+			System.out.println("i等于"+i);
+		}
+		
+	}
+
+}
