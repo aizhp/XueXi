078be85003039c4e4a55edac18bdacf75ed28d45
aizhp <aizhp@users.noreply.github.com>
2017-08-06 23:10:29 +0800

    8.6

---
+++ src/Xue/Xarray.java
@@ -0,0 +1,19 @@
+package Xue;
+
+public class Xarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a=new int[]{1,2,3,4,5,6,7,8,9,0};
+		int phone = a[0];
+		System.out.println(phone);
+		int num = 0;
+		int i=1;
+		for(i=1;i<=999;i++){
+			num += i;
+			
+		}
+		System.out.println(num);
+	}
+
+}
